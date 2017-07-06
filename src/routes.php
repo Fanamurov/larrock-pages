c0636474a9d79fa8f8fd706801368bdb32bdf80a
Alexandr Fanamurov <fanamura@gmail.com>
2017-07-06 13:21:07 +1000

    Fix

--- src/routes.php
+++ src/routes.php
@@ -16,6 +16,6 @@ Route::group(['middleware' => $middlewares], function(){
     ]);
 });
 
-Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu']], function(){
+Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData']], function(){
     Route::resource('page', AdminPageController::class);
 });
