59f15692e988ebdd6a77ecc8bf7a960442785518
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-31 15:12:38 +1000

    +searchSiteAdmin Middleware

--- src/routes.php
+++ src/routes.php
@@ -19,6 +19,6 @@ Route::group(['middleware' => $middlewares], function(){
     ]);
 });
 
-Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData']], function(){
+Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData', 'SiteSearchAdmin']], function(){
     Route::resource('page', AdminPageController::class);
 });
