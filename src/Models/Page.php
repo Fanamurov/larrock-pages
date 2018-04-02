1a032d8bfe2c03a141ed4af2a9f46404b0384983
Alexandr Fanamurov <fanamurov@yandex.ru>
2018-04-02 07:30:51 +0000

    Apply fixes from StyleCI

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -2,23 +2,23 @@
 
 namespace Larrock\ComponentPages\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Larrock\Core\Helpers\Plugins\RenderPlugins;
-use Larrock\Core\Traits\GetFilesAndImages;
-use Larrock\Core\Traits\GetLink;
-use Larrock\Core\Traits\GetSeo;
-use Nicolaslopezj\Searchable\SearchableTrait;
-use Spatie\MediaLibrary\HasMedia\HasMedia;
-use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Cache;
 use LarrockPages;
 use Larrock\Core\Component;
-use Cache;
+use Larrock\Core\Traits\GetSeo;
+use Larrock\Core\Traits\GetLink;
 use Spatie\MediaLibrary\Models\Media;
+use Illuminate\Database\Eloquent\Model;
+use Larrock\Core\Traits\GetFilesAndImages;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Nicolaslopezj\Searchable\SearchableTrait;
+use Larrock\Core\Helpers\Plugins\RenderPlugins;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
 /**
- * Larrock\ComponentPages\Models\Page
+ * Larrock\ComponentPages\Models\Page.
  *
- * @property integer $id
+ * @property int $id
  * @property string $title
  * @property string $category
  * @property string $short
@@ -26,7 +26,7 @@ use Spatie\MediaLibrary\Models\Media;
  * @property string $url
  * @property string $date
  * @property string $position
- * @property integer $active
+ * @property int $active
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  * @property mixed $description_render
@@ -65,13 +65,13 @@ class Page extends Model implements HasMedia
 
     protected $searchable = [
         'columns' => [
-            'page.title' => 10
-        ]
+            'page.title' => 10,
+        ],
     ];
 
     protected $casts = [
         'position' => 'integer',
-        'active' => 'integer'
+        'active' => 'integer',
     ];
 
     protected $dates = ['created_at', 'updated_at', 'date'];
@@ -85,49 +85,53 @@ class Page extends Model implements HasMedia
     {
         return $query->where('active', 1);
     }
-    
+
     public function getFullUrlAttribute()
     {
-        return '/page/'. $this->url;
+        return '/page/'.$this->url;
     }
 
     /**
-     * Замена тегов плагинов на их данные
+     * Замена тегов плагинов на их данные.
      * @return mixed
      * @throws \Throwable
      */
     public function getDescriptionRenderAttribute()
     {
-        $cache_key = 'DescriptionRender'. $this->config->table.'-'. $this->id;
-        if(\Auth::check()){
-            $cache_key .= '-'. \Auth::user()->role->first()->level;
+        $cache_key = 'DescriptionRender'.$this->config->table.'-'.$this->id;
+        if (\Auth::check()) {
+            $cache_key .= '-'.\Auth::user()->role->first()->level;
         }
-        return Cache::rememberForever($cache_key, function(){
+
+        return Cache::rememberForever($cache_key, function () {
             $renderPlugins = new RenderPlugins($this->description, $this);
             $render = $renderPlugins->renderBlocks()->renderImageGallery()->renderFilesGallery();
+
             return $render->rendered_html;
         });
     }
 
     /**
-     * Перезаписываем метод из HasMediaTrait, добавляем кеш
+     * Перезаписываем метод из HasMediaTrait, добавляем кеш.
      * @param string $collectionName
      * @return mixed
      */
     public function loadMedia(string $collectionName)
     {
-        $cache_key = sha1('loadMediaCache'. $collectionName . $this->id . $this->getConfig()->getModelName());
+        $cache_key = sha1('loadMediaCache'.$collectionName.$this->id.$this->getConfig()->getModelName());
+
         return Cache::rememberForever($cache_key, function () use ($collectionName) {
             $collection = $this->exists
                 ? $this->media
                 : collect($this->unAttachedMediaLibraryItems)->pluck('media');
 
             return $collection->filter(function (Media $mediaItem) use ($collectionName) {
-                    if ($collectionName === '') {
-                        return true;
-                    }
-                    return $mediaItem->collection_name === $collectionName;
-                })->sortBy('order_column')->values();
+                if ($collectionName === '') {
+                    return true;
+                }
+
+                return $mediaItem->collection_name === $collectionName;
+            })->sortBy('order_column')->values();
         });
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -2,10 +2,10 @@
 
 Route::get('/page/{url}', 'Larrock\ComponentPages\PageController@getItem')->name('page');
 
-Route::group(['prefix' => 'admin'], function(){
+Route::group(['prefix' => 'admin'], function () {
     Route::resource('page', 'Larrock\ComponentPages\AdminPageController');
 });
 
-Breadcrumbs::register('admin.'. LarrockPages::getName() .'.index', function($breadcrumbs){
-    $breadcrumbs->push(LarrockPages::getTitle(), '/admin/'. LarrockPages::getName());
+Breadcrumbs::register('admin.'.LarrockPages::getName().'.index', function ($breadcrumbs) {
+    $breadcrumbs->push(LarrockPages::getTitle(), '/admin/'.LarrockPages::getName());
 });
